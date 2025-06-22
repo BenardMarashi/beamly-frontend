2e084570f5bb6a6c64ea3c8559ec53e688287ea3
BenardMarashi <117479668+BenardMarashi@users.noreply.github.com>
2025-06-22 20:13:37 +0200

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,43 @@ import { heroui } from "@heroui/react";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		"./index.html",
-		"./src/**/*.{js,ts,jsx,tsx}",
-		"./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-		extend: {
-			colors: {
-				beamly: {
-					primary: "#0F43EE",
-					secondary: "#FCE90D",
-					third: "#011241"
-				}
-			}
-		},
-	},
-	darkMode: "class",
-	plugins: [heroui()],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        beamly: {
+          primary: "#0F43EE",
+          secondary: "#FCE90D",
+          third: "#011241"
+        }
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [
+    heroui({
+      themes: {
+        light: {
+          colors: {
+            secondary: {
+              DEFAULT: "#FCE90D",
+              foreground: "#011241", // text color on secondary background
+            },
+          },
+        },
+        dark: {
+          colors: {
+            secondary: {
+              DEFAULT: "#FCE90D",
+              foreground: "#011241", // text color on secondary background
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
